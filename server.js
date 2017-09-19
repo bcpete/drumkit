a2188283dd0397e5e2a907c740e46ba270f4ff2c
hometownzero <bradycpeters@gmail.com>
2017-09-19 17:08:48 -0500

    and a node server to serve the page

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+const path = require('path'); //
 
 const publicPath = path.join(__dirname + '/public');
 const port = process.env.PORT || 3000;
